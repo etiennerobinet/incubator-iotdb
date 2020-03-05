531b1ae1a9e7df243f8e8a81d36ab79af09151d2
Etienne Robinet <etienne1995robinet@gmail.com>
2020-03-05 14:20:58 +0100

    Camel component and OSGi JDBC

---
+++ camel-iotdb/src/main/java/org/apache/iotdb/camel/IoTDBEndpoint.java
@@ -0,0 +1,148 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+ or more contributor license agreements.  See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership.  The ASF licenses this file
+ to you under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License.  You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing,
+ software distributed under the License is distributed on an
+ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ KIND, either express or implied.  See the License for the
+ specific language governing permissions and limitations
+ under the License.
+ */
+package org.apache.iotdb.camel;
+
+import main.java.org.apache.iotdb.camel.IoTDBConsumer;
+import org.apache.camel.Component;
+import org.apache.camel.Consumer;
+import org.apache.camel.Processor;
+import org.apache.camel.Producer;
+import org.apache.camel.api.management.ManagedAttribute;
+import org.apache.camel.impl.DefaultEndpoint;
+import org.apache.camel.spi.Metadata;
+import org.apache.camel.spi.UriEndpoint;
+import org.apache.camel.spi.UriParam;
+import org.apache.camel.spi.UriPath;
+import org.apache.iotdb.session.Session;
+
+import java.io.UnsupportedEncodingException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+@UriEndpoint(scheme = "iotdb", title = "IoTDB", syntax = "iotdb:url", label = "iotdb")
+public class IoTDBEndpoint extends DefaultEndpoint {
+    @UriPath @Metadata(required = "true")
+    private String url;
+
+    @UriParam @Metadata(required = "false")
+    private  String sql;
+
+    @UriParam
+    @Metadata(required = "false")
+    @SuppressWarnings("unused")
+    private String username;
+
+    @UriParam
+    @Metadata(required = "false")
+    @SuppressWarnings("unused")
+    private String password;
+
+    private final int IOTDB_PORT=6667;
+    private Session session;
+
+    private final Pattern ipAdd = Pattern.compile("(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)");
+
+
+    public IoTDBEndpoint(String uri, Component ioTDBComponent) {
+        super(uri, ioTDBComponent);
+        url = extractIp(uri);
+        if (username != null && password != null) {
+            session = new Session(url, IOTDB_PORT, username, password);
+        } else {
+            session = new Session(url, IOTDB_PORT, "root", "root");
+        }
+    }
+
+    private String extractIp(String uri) {
+        Matcher matcher = ipAdd.matcher(uri);
+        if(matcher.find()){
+            return matcher.group();
+        }
+        else return "0.0.0.0";
+    }
+
+    @Override
+    public void doStart() throws Exception {
+        super.doStart();
+        if(url==null){
+            url=getEndpointUri();
+            System.out.println("URL: "+url);
+        }
+    }
+
+    @ManagedAttribute
+    public String getSql() {
+        return sql;
+    }
+
+    @ManagedAttribute
+    public void setSql(String sql) throws UnsupportedEncodingException {
+        this.sql = java.net.URLDecoder.decode(sql, "UTF-8");
+    }
+
+    public Session getSession() {
+        return session;
+    }
+
+    @Override
+    public Producer createProducer() throws Exception {
+        return new IoTDBProducer(this);
+    }
+
+    @Override
+    public Consumer createConsumer(Processor processor) throws Exception {
+        return new IoTDBConsumer(this,processor);
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return false;
+    }
+
+    @ManagedAttribute
+    public String getUrl() {
+        return url;
+    }
+
+    @ManagedAttribute
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @ManagedAttribute
+    public String getUsername() {
+        return username;
+    }
+
+    @ManagedAttribute
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @ManagedAttribute
+    public String getPassword() {
+        return password;
+    }
+
+    @ManagedAttribute
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ camel-iotdb/src/main/java/org/apache/iotdb/camel/IoTDBProducer.java
@@ -0,0 +1,68 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+ or more contributor license agreements.  See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership.  The ASF licenses this file
+ to you under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License.  You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing,
+ software distributed under the License is distributed on an
+ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ KIND, either express or implied.  See the License for the
+ specific language governing permissions and limitations
+ under the License.
+ */
+package org.apache.iotdb.camel;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.impl.DefaultProducer;
+import org.apache.iotdb.session.IoTDBSessionException;
+import org.apache.iotdb.session.Session;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class IoTDBProducer extends DefaultProducer {
+
+    private final String update;
+    private Session session;
+    public IoTDBProducer(IoTDBEndpoint endpoint) {
+        super(endpoint);
+        session = endpoint.getSession();
+        update= endpoint.getSql();
+    }
+
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        /**Message in = exchange.getIn();
+        String device = in.getHeader("device",String.class);
+        List<String> sensor = in.getHeader("sensor",List.class);
+        List<String> value = in.getHeader("value",List.class);
+        session.insert(device,0,sensor,value);*/
+
+        session.executeNonQueryStatement(update);
+    }
+
+    @Override
+    public void doStart(){
+        try {
+            session.open();
+        } catch (IoTDBSessionException e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public void doStop(){
+        try {
+            session.close();
+        } catch (IoTDBSessionException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
